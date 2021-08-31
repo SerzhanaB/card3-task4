0255b4d857fffc74402ce69077bb1842ceb9adcf
Serzhana <buraevase@gmail.com>
2021-09-01 01:16:35 +0300

    add info

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 //import logo from './logo.svg';
 import "./App.css";
+import pic from "./lake.jpg";
+import pic2 from "./green.jpg";
 
 export default function App() {
-  const newsCard = ({title, desc}) => {
+ 
+  const massiveData =[
+
+    { title: "Иркутск, Иркутская область", desc: "Средние температуры холодного периода года варьируют от -5 °C до -40 °C, а теплого от 10 °C до 35 °C" },
+     {title: "Улан-Удэ, Респ. Бурятия", desc: "Средняя температура воздуха зимой в Улан-Удэ в дневные часы составляет -17 °C, в то время как вечером падает до -19 °C." },
+    { title: "Чита, Забайкальский край", desc: "Зимой погода в Чите преимущественно холодная, средняя температура воздуха составляет -17,3°C." }
+  ]
+ 
+ 
+ 
+ 
+ 
+  const newsCard = ({ title, desc }) => {
     return (
-      <div>
+      <div
+      style={{
+        boxSizing:'border-box',
+        padding: '10px',
+        border: '1px solid green',
+        width:'30%',
+        borderRadius:'10px',
+       backgroundColor: '#a3e2f7',
+        
+        
+    }} >
         <h1>{title}</h1>
         <p>{desc}</p>
       </div>
     );
   };
   return (
+    <body style={{
+      backgroundImage: `url(${pic2})`
+    }}>
     <div className="App">
-      <img src="green.jpg" />
-      {newsCard({ title: 'Новость 1', desc: 'ntrcn' })}
-      {newsCard({ title: 'Новость 2', desc: 'ferfegerergege' })}
-      {newsCard({ title: 'Новость 3', desc: 'ewfrwefwefewf' })}
+      <img
+        style={{maxWidth: '100%',
+        }}
+        src={pic}
+        alt="текст"
+      />
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: 'space-between',
+          
+        }}
+      >
+      {massiveData.map(props=> newsCard(props))}
+      </div>
     </div>
+
+    </body>
   );
 }
